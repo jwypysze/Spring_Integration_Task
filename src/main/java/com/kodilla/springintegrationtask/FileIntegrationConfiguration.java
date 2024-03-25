73f07a708ccf4f82c6732113beabd23e8e5482c4
jwypysze <joanna.wypyszewska@gmail.com>
2024-03-25 20:33:35 +0100

    Refactoring

--- src/main/java/com/kodilla/springintegrationtask/FileIntegrationConfiguration.java
+++ src/main/java/com/kodilla/springintegrationtask/FileIntegrationConfiguration.java
@@ -2,20 +2,14 @@ package com.kodilla.springintegrationtask;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.expression.Expression;
-import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
 import org.springframework.integration.dsl.Pollers;
 import org.springframework.integration.file.FileReadingMessageSource;
 import org.springframework.integration.file.FileWritingMessageHandler;
-import org.springframework.integration.file.support.FileExistsMode;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 @Configuration
 public class FileIntegrationConfiguration {
